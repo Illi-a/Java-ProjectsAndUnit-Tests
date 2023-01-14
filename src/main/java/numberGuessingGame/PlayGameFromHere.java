3741372bd8b9d52cd902d60fb4412ed2f0f41790
Illia Lytvychenko <illia.lytvychenko@gmail.com>
2023-01-14 23:39:48 +0200

    3 mini projects added, even mini-game

--- src/main/java/numberGuessingGame/PlayGameFromHere.java
+++ src/main/java/numberGuessingGame/PlayGameFromHere.java
@@ -4,8 +4,6 @@ public class PlayGameFromHere {
     public static void main(String[] args) {
         NumberGuessingGame numberGuessingGame = new NumberGuessingGame();
         numberGuessingGame.help();
-        numberGuessingGame.startGame("Intermediate");
-
-
     }
 }
+
